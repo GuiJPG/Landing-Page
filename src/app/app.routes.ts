e729267ca522473f90c3427efaf95c8b8ae5557e
GuiJPG <oguilherme.rodrigues10@gmail.com>
2024-02-19 22:01:07 -0300

    Criando header e configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 //Controla as rotas das aplicações - home, contatos, ajuda etc.
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: "",
+    component: HomeComponent
+  }
+
+];
