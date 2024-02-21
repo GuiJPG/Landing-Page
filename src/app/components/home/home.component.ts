c678f37bf09146c54b68369135f3e3495fa58551
GuiJPG <oguilherme.rodrigues10@gmail.com>
2024-02-21 00:56:00 -0300

    Finalização do projeto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,12 +3,14 @@ import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent
+    HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsletterFormComponent, BenefitComponent, FooterComponent
   ],
   providers: [],
   templateUrl: './home.component.html',
